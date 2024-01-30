6accc3cff9aff519b1f37fc81ff918decfedb676
Thanh Tran <trongthanh@gmail.com>
2024-01-30 09:57:39 +0700

    feat: add maskable icon

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,14 @@ export default {
 		minifyTaggedTemplate(), // minify html & css tagged templates
 		VitePWA({
 			registerType: 'autoUpdate',
-			includeAssets: ['favicon.ico', 'app-icon.svg', 'app-icon.png', 'app-icon-256.png'],
+			includeAssets: ['favicon.ico', 'app-icon.svg', 'app-icon.png', 'app-icon-maskable.png'],
 			manifest: {
 				name: 'Âm Lịch PWA',
 				short_name: 'Âm Lịch',
 				description:
 					'Âm Lịch Việt Nam Progressive Web App, phát triển bởi Trần Trọng Thanh, dựa trên thuật toán tính âm lịch của Hồ Ngọc Đức.',
 				theme_color: '#212121',
+				background_color: '#3b82f6',
 				icons: [
 					{
 						src: 'app-icon.png',
@@ -20,9 +21,10 @@ export default {
 						type: 'image/png',
 					},
 					{
-						src: 'app-icon-256.png',
-						sizes: '256x256',
+						src: 'app-icon-maskable.png',
 						type: 'image/png',
+						sizes: '512x512',
+						purpose: 'maskable',
 					},
 				],
 			},
