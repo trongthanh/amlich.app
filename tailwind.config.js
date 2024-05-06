bbec773591b6278a0fa4091854d257932d7c1ca1
Thanh Tran <trongthanh@gmail.com>
2024-05-06 22:34:12 +0700

    feat: switch style solution to tailwind css to make use of its atomic compiler

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['src/**/*.html'],
+	theme: {
+		fontFamily: {
+			sans: [
+				'Inter',
+				'system-ui',
+				'-apple-system',
+				'Segoe UI',
+				'Roboto',
+				'Helvetica Neue',
+				'Noto Sans',
+				'Liberation Sans',
+				'Arial',
+				'sans-serif',
+			],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
